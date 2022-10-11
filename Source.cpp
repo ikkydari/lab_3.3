478bd83fb0ab4895ff7665cf5659c75833fe7a06
ikkydari <darinaparastatova@ukr.net>
2022-10-11 14:08:23 +0300

    LR3.3

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+// Lab_03_3.cpp
+// < Парастатова Дарина >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 16
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент66
+	double R1; // вхідний параметр
+	double R2; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R1 = "; cin >> R1;
+	cout << "R2 = "; cin >> R2;
+	cout << "x = "; cin >> x;
+	// розгалуження в повній формі
+	if (x <= -R1)
+		y = (R1 * x + 2 * R1) / (R1 - 2);
+	else
+		if (-R1 < x && x <= 0)
+			y = sqrt(pow(R1, 2) - pow(x, 2)) - R1;
+		else
+			if (0 < x && x <= R2)
+				y = R2 - sqrt(pow(R2, 2) - pow(x, 2));
+			else
+				if (R2 < x && x <= 4)
+					y = -R1;
+				else
+					y = (R1 * x - 2 * R1) / 2;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	system("pause");
+	return 0;
+}
